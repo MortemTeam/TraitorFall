5932247bb2bc9a6e22b0a59600fef9555f7bb3fd
HentaiStorm <60922927+HentaiStorm@users.noreply.github.com>
2022-01-04 13:15:17 +0500

    refactor code

--- spyfall/bot.py
+++ spyfall/bot.py
@@ -1,13 +1,15 @@
 from configparser import ConfigParser
 import json
 
-from discord import User, Embed, Message, RawReactionActionEvent
+from discord import Member, Embed, Message, RawReactionActionEvent
 from discord.ext import tasks
 from discord.ext.commands import Bot, Context, CommandNotFound
 
 import terminal
 import states
 import buttons
+
+from game import Game
 
 config = ConfigParser(allow_no_value=True)
 config.read("config.ini")
@@ -17,69 +19,12 @@ terminal.info("Initializing...")
 bot = Bot(command_prefix=config.get("AUTH", "PREFIX"))
 
 
-class Game:
-    guild = channel = None
-    admins = []
-    intents = []
-
-    locations = {}
-    current_location = None
-    antagonist = None
-
-    state = states.IDLE
-    message = None
-    lobby = []
-
-    @classmethod
-    def reset(cls):
-        cls.state = states.IDLE
-        cls.message = None
-        cls.lobby.clear()
-
-    @classmethod
-    def get_embed(cls):
-        return Embed(
-            title=cls.get_title(),
-            description=cls.get_description(),
-            colour=cls.state,
-        ).set_footer(
-            text="У вас две минуты чтобы присоединиться к игре...",
-        )
-
-    @classmethod
-    def get_title(cls):
-        if cls.state == states.IDLE:
-            return "Игра остановлена!"
-        if cls.state == states.LOBBY:
-            return "Игра начинается!"
-        if cls.state == states.GAME:
-            return "Игра уже идет!"
-
-    @classmethod
-    def get_description(cls):
-        return (
-            "{} - Присоединиться к игре\n"
-            "{} - Получить список локаций\n"
-            "{} - Перезапустить таймер\n"
-            "{} - Начать игру\n"
-            "{} - Закрыть лобби\n"
-            "\nИгроки:\n"
-            "{}"
-        ).format(
-            *cls.intents,
-            "\n".join("> {}. {}#{}".format(i, x.nick, x.discriminator) for i, x in enumerate(cls.lobby, 1))
-            if cls.lobby else "'¯\_(ツ)_/¯"
-        )
-
-## READY ##
-
-
 @bot.event
 async def on_ready():
     Game.guild = await bot.fetch_guild(config.getint("GUILD", "GUILD_ID"))
     Game.channel = await bot.fetch_channel(config.getint("GUILD", "CHANNEL_ID"))
     Game.admins = [
-        await Game.guild.fetch_member(x) for x in filter(lambda x: x, config.get("GUILD", "ADMINS").split(" "))
+        await Game.guild.fetch_member(int(x)) for x in filter(lambda x: x, config.get("GUILD", "ADMINS").split(" "))
     ]
     Game.intents = [buttons.JOIN, buttons.LOCATIONS, buttons.RESTART, buttons.START, buttons.CLOSE]
 
@@ -87,7 +32,7 @@ async def on_ready():
     with open(config.get("GAME", "LOCATIONS_FILE"), encoding='utf8') as file:
         Game.locations = json.loads(file.read())
 
-    terminal.info("Guild: %s" % Game.guild.name)
+    terminal.info("Guild: %s (%s)" % (Game.guild.name, len(Game.guild.members)))
     terminal.info("Channel: %s/%s" % (Game.channel.category, Game.channel.name))
     terminal.info("Admins: %s" % ", ".join("{}#{}".format(x.nick, x.discriminator) for x in Game.admins))
     terminal.successful("Started: %s" % bot.user)
@@ -96,9 +41,6 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx: Context, err: CommandNotFound):
     await ctx.reply(embed=Embed(description=err, colour=Game.state))
-
-
-## ## ##
 
 
 @bot.group()
@@ -124,7 +66,7 @@ async def on_raw_reaction_add(payload: RawReactionActionEvent):
     if payload.user_id == bot.user.id or payload.channel_id != Game.channel.id:
         return
 
-    user: User = await Game.guild.fetch_member(payload.user_id)
+    user: Member = await Game.guild.fetch_member(payload.user_id)
     message: Message = await Game.channel.fetch_message(payload.message_id)
     if payload.emoji.name != buttons.JOIN:
         await message.remove_reaction(payload.emoji, user)
@@ -133,9 +75,8 @@ async def on_raw_reaction_add(payload: RawReactionActionEvent):
         await user.send(embed=Embed(
             title="Список локаций и ролей:",
             description="\n\n".join(
-                "{}. {}\n{}".format(i, x['Location'],
-                    "\n".join("- {}".format(y) for y in x['Roles'])
-                ) for i, x in enumerate(Game.locations, 1)
+                "{}. {}\n{}".format(i, x['Location'], "\n".join("- {}".format(y) for y in x['Roles']))
+                for i, x in enumerate(Game.locations, 1)
             )))
 
     if message == Game.message:
@@ -167,7 +108,7 @@ async def on_raw_reaction_remove(payload: RawReactionActionEvent):
 
     message: Message = await Game.channel.fetch_message(payload.message_id)
     if message == Game.message:
-        user: User = await Game.guild.fetch_member(payload.user_id)
+        user: Member = await Game.guild.fetch_member(payload.user_id)
 
         if payload.emoji.name == buttons.JOIN:
             Game.lobby.remove(user)
@@ -175,7 +116,7 @@ async def on_raw_reaction_remove(payload: RawReactionActionEvent):
             return
 
 
-@tasks.loop(seconds=120)
+@tasks.loop(seconds=120, count=2)
 async def close_lobby(message: Message):
     if close_lobby.false_start:
         close_lobby.false_start = False
@@ -187,6 +128,7 @@ async def close_lobby(message: Message):
 
     close_lobby.false_start = True
     close_lobby.cancel()
+
 
 close_lobby.false_start = True
 bot.run(config.get("AUTH", "TOKEN"))
---
+++ spyfall/gamee.py
@@ -0,0 +1,158 @@
+import asyncio
+import random
+import sys
+import os
+import json
+import time
+
+import config
+from discord import Embed
+
+SPY = "Traitor"
+
+# object to keep track of players in the game
+class Player:
+    def __init__(self, channel):
+        # discord messagable pipe
+        self.channel = channel
+        
+        # game role
+        self.role = None
+
+
+# object to keep track of game state
+class Game:
+    def __init__(self, bot):
+        self.bot = bot
+
+        # locations, roles and other json settings
+        if os.path.exists(config.LOCATIONS_FILE):
+            with open(config.LOCATIONS_FILE, 'r') as locations_file:
+                self._game_data = json.load(locations_file)
+        else:
+            print("The file %s does not exist or cannot be opened" % config.LOCATIONS_FILE)
+            sys.exit(-1)
+        
+        # location list
+        self.loc_list = self._game_data['locations']
+        
+        # messagable pipe
+        self.channel = None
+        self.guild   = None
+        
+        # list of player objects
+        self.players = []
+
+        # keeps track of the current location (int index of locations json key)
+        self.location = None
+
+        # game is live only when it is started, by default it is not
+        self.is_live = False
+
+        # the amount of time in seconds available in each round (default: 480 seconds or 8 minutes)
+        self.round_time = 120
+
+        # time left in seconds
+        self.time_left = self.round_time
+
+    # add player to game
+    async def join_player(self, user_channel):
+        for player in self.players:
+            if player.channel == user_channel:
+                return
+    
+        self.players.append(Player(user_channel))
+        await self.channel.send(embed=Embed(description="<@%s> присоединяется к игре" % user_channel.id, colour=config.PREGAME))
+        
+    # remove player from game
+    async def leave_player(self, user_channel):
+        for player in self.players:
+            if player.channel == user_channel:
+                self.players.remove(player)
+                return await self.channel.send(embed=Embed(description="<@%s> покидает игру." % user_channel.id, colour=config.PREGAME))
+    
+    # get player by pipe
+    def get_player(self, user_channel):
+        for player in self.players:
+            if player.channel == user_channel:
+                return player
+    
+    async def show_locations(self, user_channel):
+        await user_channel.send(embed=Embed(
+            colour=config.SHOW_CREDITS,
+            title="TraitorFall - Locations List", 
+            description="\n".join("%s. **%s**" % (i, l['Location']) for i, l in enumerate(self.loc_list, 1))))        
+    
+    # show player to pipe
+    async def show_player(self):
+        playing = 'Текущие игроки:\n'
+        for i, player in enumerate(self.players, 1):
+            playing += "%s. **%s#%s**\n" % (i, player.channel.display_name, player.channel.discriminator)
+
+        await self.channel.send(embed=Embed(description=playing, colour=config.PREGAME))
+    
+    # make alive while, show roles, locations
+    async def start_game(self):
+        if len(self.players) < 3:
+            return await self.channel.send('Мало игроков.')
+
+        self.is_live = True
+        self.time_left = time.time() + self.round_time * len(self.players)
+        self.location = random.choice(self.loc_list)
+        random.choice(self.players).role = SPY
+        
+        await self.channel.send("Игра начинается!")
+    
+        locs = await self.channel.send(embed=Embed(
+            colour=config.SHOW_CREDITS,
+            title="TraitorFall - Locations List", 
+            description="\n".join("%s. **%s**" % (i, l['Location']) for i, l in enumerate(self.loc_list, 1))))
+        
+        title = "TraitorFall - Game"
+        for player in self.players:
+            description = "Ваша роль ---> **ПРЕДАТЕЛЬ** \n Локация ---> *???*"
+            if player.role != SPY:
+                player.role = random.choice(self.location['Roles'])
+                description = "Ваша роль ---> **%s** \nЛокация ---> *%s*" % (player.role, self.location['Location'])
+
+            await player.channel.send(embed=Embed(title=title, description=description, colour=config.SHOW_CREDITS))        
+        
+        while self.is_live and time.time() < self.time_left:
+            await asyncio.sleep(1)
+
+        # Loop exited, game has ended or has run out of time. End it and clear messages.
+        await locs.delete()
+
+        # If game is still live, it means the spy has not been revealed yet even though the time is up.
+        # Players still have a last chance to vote who the spy is before ending the game.
+        if self.is_live:
+            await self.channel.send("Время вышло! Укажите шпиона.")        
+
+    # take end by Traitor
+    async def choice_location(self, user_channel, choice):
+        player = self.get_player(user_channel)
+        if player.role != SPY or not(choice.isdigit()) or not(0 < int(choice) < len(self.loc_list) + 1):
+            await self.channel.send(embed=Embed(description="<@%s> наказан" % user_channel.id, colour=config.ANTITRAIT))
+            role = self.guild.get_role(config.ANTITRAITOR)
+            return await user_channel.add_roles(config.ANTITRAITOR)
+        
+        await self.end_game(1)
+        
+        description = "<@%s> тренируйся лучше, ты проиграл!"
+        if self.loc_list[int(choice)-1] == self.location:
+            description = "<@%s> победил в игре!"
+        await self.channel.send(embed=Embed(description=description % user_channel.id, colour=config.SHOW_END))
+
+    # reveal identity of the spy
+    async def end_game(self, force=0):
+        if force or time.time() > self.time_left:
+            self.is_live = False
+
+            title = 'TraitorFall - Reveal'
+            description = 'Локацией было --> **%s**\n\n' % self.location['Location']
+        
+            for i, player in enumerate(self.players, 1):
+                description += "%s. **%s#%s** (%s)\n" % (i, player.channel.display_name, player.channel.discriminator, player.role)
+    
+            self.players.clear()
+            await self.channel.send(embed=Embed(title=title, description=description, colour=config.SHOW_END))
